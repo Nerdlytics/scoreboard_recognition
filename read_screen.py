ade991110020ba3900a81f42abc645145d5aaddd
David Schappler <dnschappler@gmail.com>
2019-12-29 14:24:36 -0500

    changed processing methods

--- read_screen.py
+++ read_screen.py
@@ -227,8 +227,8 @@ def process_img(image):
     # convert to gray
     processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     processed_img = cv2.fastNlMeansDenoising(processed_img)
-    ret,processed_img = cv2.threshold(processed_img, 127, 255, cv2.THRESH_BINARY_INV)
-    #processed_img = cv2.adaptiveThreshold(processed_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    #ret,processed_img = cv2.threshold(processed_img, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    processed_img = cv2.adaptiveThreshold(processed_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 6)
     processed_img = cv2.fastNlMeansDenoising(processed_img)
 
     return processed_img
